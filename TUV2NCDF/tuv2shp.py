7256a61fa1772852539eedb156712d9efa5d818d
Pedro Montero <pmontero@intecmar.gal>
2020-07-22 15:34:12 +0200

    Initial src

---
+++ TUV2NCDF/tuv2shp.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+**tuv2shp.py**
+
+* *Purpose:* Create a shapefile from a HF Radar file from the Combine, format TUV
+
+* *python version:* 3.7
+* *author:* Pedro Montero
+* *license:* INTECMAR
+* *requires:*
+
+* *date:* 2020/07/22
+
+* *version:* 0.0.1
+* *version date:* 2020/07/22
+
+"""
+
+import shapefile
+
+
+def read_tuv():
+    pass
